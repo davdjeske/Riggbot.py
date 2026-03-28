a41a9f19146d792fd666a9d968e5b45e332689f5
David Jeske <davdjeske@gmail.com>
2026-03-27 19:49:26 -0500

    i hate async

--- riggbot.py
+++ riggbot.py
@@ -334,20 +334,20 @@ async def translate_text(text: str, is_manual: bool) -> str | None:
         for i, link in enumerate(md_links):
             text = text.replace(link, f'{{LINK_{i}}}', 1)
 
-        detected = translator.detect(text)
+        detected = await translator.detect(text)
 
         if detected.lang != DEST_LANG:
             # need to translate
             logging.info(
                 f'Translating from {detected.lang} to {DEST_LANG}')
-            translated = translator.translate(text, dest=DEST_LANG)
+            translated = await translator.translate(text, dest=DEST_LANG)
             translation = f"{detected.lang}→{DEST_LANG}: {translated.text}"
 
         elif is_manual:
             # if manual and already in dest lang, translate to override lang
             logging.info(
                 f'Override: translating from {DEST_LANG} to {MANUAL_OVERRIDE_LANG}')
-            translated = translator.translate(text, dest=MANUAL_OVERRIDE_LANG)
+            translated = await translator.translate(text, dest=MANUAL_OVERRIDE_LANG)
             translation = translated.text
 
         # Restore extracted markdown links
